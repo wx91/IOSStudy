ee162ae5cf1f2adb5146fbb5134438d3a7f322d7
wx91 <766221790@qq.com>
2015-05-29 22:47:31 +0800

    是swift语言重写crazyDrag这个简单的游戏

---
+++ CragDrag/CragDrag/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CragDrag
+//
+//  Created by wangx on 15/5/29.
+//  Copyright (c) 2015年 wxiang1991. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
